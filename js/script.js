1f741c38e117e64bd0fb49e821ccdf2d23a979bf
luciameneghin <lucia.meneghin00@gmail.com>
2024-12-06 15:45:05 +0100

    fix: milestone-1 e milestone-2

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ let counter = 0;
 photo()
 
 function photo() {
-  // photoArray = [];
   errorEl.innerHTML = '';
 
   axios.get(endpoint)
@@ -23,7 +22,7 @@ function photo() {
       const photoArray = response.data.map(photo => {
         return { title: photo.title, url: photo.url };  // Estraggo title e url di tutte le img(destrutturazione)
       });
-
+      console.log(photoArray);
       showAllCards(photoArray);
     })
     .catch(e => {
@@ -47,11 +46,14 @@ function showAllCards(photoArray) {
 // prepara tutte le card
 function showCards(card) {
   const { title, url } = card;
-  return `<div class="single-user-card col-4 mx-3 my-3">
-            <img src="assets_day1/img/pin.svg" alt="pin" class="pin">
-            <img src="${url}" alt=".." class="my-3">
-            <p class="text-card">${title}</p>
-          </div>`;
+  return `
+            <div class="single-user-card col-4 mx-3 my-3">
+              <img src="assets_day1/img/pin.svg" alt="pin" class="pin">
+              <img src="${url}" alt=".." class="my-3">
+              <p class="text-card">${title}</p>
+            </div>
+          `
+
 }
 
 // messaggio di errore
@@ -63,16 +65,25 @@ function printError(errorMessage) {
 // giorno 2
 const overlayImgBtn = document.querySelector('.overlay');
 const button = document.querySelector('.btn');
-// const singleUserCard = document.querySelectorAll('.single-user-card')
-console.log(showCards);
+const imageOverlay = document.querySelector('.img-overlay')
 
 
 button.addEventListener('click', () => {
   overlayImgBtn.classList.add('d-none')
 })
 
-userCard.addEventListener('click', () => {
-  userCard.classList.add('d-none')
-  overlayImgBtn.classList.remove('d-none')
-  userCard.classList.remove('d-none')
-})
+userCard.addEventListener('click', (event) => {
+  // Controlla che l'elemento cliccato sia una card (dentro la userCard)
+  const card = event.target.closest('.single-user-card');
+
+  if (card) {
+    const imageUrl = card.getAttribute('data-url');  // Ottieni l'URL dell'immagine dalla card
+    showImg(imageUrl);  // Mostra l'immagine nell'overlay
+  }
+});
+
+// Funzione per mostrare l'immagine nell'overlay
+function showImg(url) {
+  imageOverlay.innerHTML = `<img src="${url}" alt="img-api" class="api-img">`;
+  overlayImgBtn.classList.remove('d-none');  // Mostra l'overlay
+}
